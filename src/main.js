54cb2f56435ccf4208864a665aae845cee83bf2b
ppojun <120106@naver.com>
2022-03-21 13:33:14 +0900

    .

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@
 // css framework : tailwind
 
 const Koa = require("koa")
-const app = new Koa()
 const path = require("path")
 const Pug = require("koa-pug")
+const websockify = require("koa-websocket")
+const route = require("koa-route")
+
+const app = websockify(new Koa())
 
 new Pug({
   viewPath: path.resolve(__dirname, "./views"),
@@ -15,5 +18,14 @@ app.use(async (ctx) => {
   ctx.body = "Hello World"
   await ctx.render("main")
 })
+
+app.ws.use(
+  route.all("/test/:id", (ctx) => {
+    ctx.websocket.send("Hello World")
+    ctx.websocket.on("message", (message) => {
+      console.log(message)
+    })
+  })
+)
 
 app.listen(3000)
