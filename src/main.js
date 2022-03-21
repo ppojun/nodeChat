020caa97ef04c4216ed13f9f8321b7cf69c65986
ppojun <120106@naver.com>
2022-03-21 09:46:08 +0900

    .

--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
+// template enginee : pug
+// css framework : tailwind
+
 const Koa = require("koa")
 const app = new Koa()
+const path = require("path")
+const Pug = require("koa-pug")
+
+new Pug({
+  viewPath: path.resolve(__dirname, "./views"),
+  app,
+})
 
 app.use(async (ctx) => {
   ctx.body = "Hello World"
+  await ctx.render("main")
 })
 
 app.listen(3000)
